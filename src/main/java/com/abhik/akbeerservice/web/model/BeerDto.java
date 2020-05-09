95fa6bf41bbc110a2d4742fcb9b1732bffb47a26
typo3biswa <typo3biswa@gmail.com>
2020-05-09 19:10:16 +0530

    added bean validations, Closes #8

--- src/main/java/com/abhik/akbeerservice/web/model/BeerDto.java
+++ src/main/java/com/abhik/akbeerservice/web/model/BeerDto.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,18 +20,34 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDto {
 	
-	private UUID id;
-	private Integer version;
-	
-	private OffsetDateTime createdDate;
-	private OffsetDateTime lastModifiedDate;
-	
-	private String beerName;
-	private BeerStyleEnum beerStyle;
-	
-	private Long upc;
-	private BigDecimal price;
-	private Integer quantityOnHand;
+	@Null
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @Null
+    private OffsetDateTime createdDate;
+
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    private String beerName;
+
+    @NotNull
+    private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
+    private Long upc;
+
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
 	
 	
 	
